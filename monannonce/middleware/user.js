5d64e5d7ae2dda7bc2eadd3a2955adddf264563c
Aboubacar23 <aboubacarsidikiconde23@gmail.com>
2024-11-20 14:08:42 +0100

    ajout de middleware

--- monannonce/middleware/user.js
+++ monannonce/middleware/user.js
@@ -1,62 +1,77 @@
-const {checkSchema} = require('express-validator');
+ const { checkSchema, validationResult } = require('express-validator');
 
-async function validatorUser(req, res, next) {
+ async function validatorUser(req, res, next) {
+     console.log('user validator');
 
-    console.log('user validator');
-    const [result] = await checkSchema({
-        nom: {
-            notEmpty : true
-        },
-    }).run(req);
+     // Exécute les validations basées sur le schéma
+     await checkSchema({
+         nom: {
+             notEmpty: {
+                 errorMessage: 'Le champ nom est requis',
+             },
+         },
+         email: {
+             notEmpty: {
+                 errorMessage: 'Le champ email est requis',
+             },
+             isEmail: {
+                 errorMessage: 'Le format de l\'email est invalide',
+             },
+         },
+     }).run(req);
+
+     // Récupère les résultats des validations
+     const errors = validationResult(req);
+     console.log('Req Body ..............')
+     console.log(req.body);
+
+     if (!errors.isEmpty())
+     {
+         return res.status(400).json({
+             statut: 'échec',
+             erreurs: errors.array(),
+         });
+     }
+
+     next();
+ }
+
+ module.exports = {
+     validatorUser,
+ };
 
 
-    console.log(req.body);
+ /** const {checkSchema} = require('express-validator');
 
-    if (!result.isEmpty())
-    {
-        res.status(400).json('Pas de nom trouver');
-    }
+  async function validatorUser(req, res, next) {
 
-    next();
-}
+  console.log('user validator');
+  const [result] = await checkSchema({
+  nom: {
+  notEmpty : true
+  },
+  email: {
+  notEmpty: true,
+  isEmail: {
+  errorMessage: 'Le format de l\'email est invalide',
+  },
+  },
+  }).run(req);
 
 
-module.exports = {
-    validatorUser
-}
+  console.log(req.body);
 
-/**
- * const { checkSchema, validationResult } = require('express-validator');
- *
- * async function validatorUser(req, res, next) {
- *     console.log('user validator');
- *
- *     // Exécute les validations basées sur le schéma
- *     await checkSchema({
- *         nom: {
- *             notEmpty: {
- *                 errorMessage: 'Le champ nom est requis',
- *             },
- *         },
- *     }).run(req);
- *
- *     // Récupère les résultats des validations
- *     const errors = validationResult(req);
- *     console.log('Req Body ..............')
- *     console.log(req.body);
- *
- *     if (!errors.isEmpty())
- *     {
- *         return res.status(400).json({
- *             statut: 'échec',
- *             erreurs: errors.array(),
- *         });
- *     }
- *
- *     next();
- * }
- *
- * module.exports = {
- *     validatorUser,
- * };
- */
+  if (!result.isEmpty())
+  {
+  res.status(400).json('Pas de nom trouver');
+  }
+
+  next();
+  }
+
+
+  module.exports = {
+  validatorUser
+  }
+  */
+
