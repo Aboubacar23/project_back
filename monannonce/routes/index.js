3f6651424ce1d55704777ea9bfdf77c21653c1fe
Aboubacar23 <aboubacarsidikiconde23@gmail.com>
2024-11-20 10:55:12 +0100

    création des models

--- monannonce/routes/index.js
+++ monannonce/routes/index.js
@@ -1,16 +1,16 @@
 const annonceRoutes = require('./annonce')
 const userRouter = require('./user')
-//const signalementRouter = require('./routes/signalement')
-//const commentaireRouter = require('./routes/commentaire')
-//const categorieRouter = require('./routes/categorie')
+const signalementRouter = require('./routes/signalement')
+const commentaireRouter = require('./routes/commentaire')
+const categorieRouter = require('./routes/categorie')
 
 function initRoutes(app) {
     //la liste classe routes
     app.use('/api/annonces', annonceRoutes);
     app.use('/api/users',userRouter);
-    //app.use('/api/categories',categorieRouter);
-    //app.use('/api/signalements',signalementRouter);
-    //app.use('/api/commentaires',commentaireRouter);
+    app.use('/api/categories',categorieRouter);
+    app.use('/api/signalements',signalementRouter);
+    app.use('/api/commentaires',commentaireRouter);
 
 }
 
