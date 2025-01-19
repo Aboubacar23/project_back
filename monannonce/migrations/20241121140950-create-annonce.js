3af764f6ec41c7b9de53e0a540c17ad0966111b3
Aboubacar23 <aboubacarsidikiconde23@gmail.com>
2025-01-19 23:14:27 +0100

    CRUD (User & Annonce )

--- monannonce/migrations/20241121140950-create-annonce.js
+++ monannonce/migrations/20241121140950-create-annonce.js
@@ -21,6 +21,9 @@ module.exports = {
       statut: {
         type: Sequelize.STRING
       },
+      image: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
